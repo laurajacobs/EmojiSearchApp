ff0531c945fffbeb2a1035502706ca8a865b30fe
maurogestoso <maurogestoso@gmail.com>
2017-06-13 10:10:06 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = React.createElement('h1', null, 'Welcome to React!');
+
+ReactDOM.render(Hello, document.getElementById('root'));
